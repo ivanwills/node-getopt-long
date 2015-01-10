5205413cc85ae1d35345240d6fcc65364b48dec2
Ivan Wills <ivan.wills@gmail.com>
2015-01-10 16:08:08 +1100

    Added mocha tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,21 @@
 var gulp   = require('gulp');
+var mocha  = require('gulp-mocha');
 var jshint = require('gulp-jshint');
+var gutil  = require('gulp-util');
 
 gulp.task('lint', function() {
     console.log('here');
     return gulp.src('./lib/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
+});
+
+gulp.task('test', function() {
+    return gulp.src(['test/*.js'], { read: false })
+        .pipe(mocha({ reporter: 'list' }))
+        .on('error', gutil.log);
+});
+
+gulp.task('watch', function() {
+    gulp.watch(['lib/**', 'test/**'], ['lint', 'test']);
 });
