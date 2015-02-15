6c694b8c97f3bee2be053720afbec3bdf2ab7a3d
Ivan Wills <ivan.wills@gmail.com>
2015-02-16 05:33:13 +1100

    Starting on top level tests

--- test/options.js
+++ test/options.js
@@ -1,47 +1,51 @@
 /* global require, describe, it, assert, beforeEach */
 var assert = require('assert');
 var getoptLong = require('../lib/getopt-long.js');
-console.log(getoptLong);
 
 var test_data = [
     {
-        'name': 'Simple',
-        'args': ['long|l', 'A long named option'],
-        'good': ['--long', '-l'],
-        'bad' : ['--log', '--q', '-q'],
+        'name'  : 'Simple',
+        'args'  : ['long|l', 'A long named option'],
+        'params': [
+        ],
+        'cmdline': [
+            { argv: ['--long'] },
+            { argv: ['-l'    ] }
+        ],
     }
 ];
 
 for ( var i in test_data ) {
     (function(data) {
         describe(data.name, function() {
-            var opt;
-            beforeEach(function() {
-                opt = getoptLong.configure(data.args);
-            });
-
-            for (var j in data.good) {
+            for (var j in data.cmdline) {
                 (function(test) {
                     it(test, function() {
-//                        opt.process(test.argv);
-//                        assert.equal(opt[name], test.result);
+                        var error, opt;
+                        try {
+                            opt = getoptLong.configure(data.args);
+                        }
+                        catch (e) {
+                            error = e;
+                        }
                     });
-                })(data.good[j]);
+                })(data.cmdline[j]);
             }
 
-            for (var k in data.bad) {
+            for (var k in data.params) {
                 (function(test) {
                     it(test, function() {
-//                        var error;
-//                        try {
-//                            opt.process(test.argv);
-//                        }
-//                        catch (e) {
-//                            error = e;
-//                        }
-//                        assert.true(!!error);
+                        var error, opt;
+                        try {
+                            opt = getoptLong.configure(data.args);
+                        }
+                        catch (e) {
+                            error = e;
+                        }
+                        assert.true(!error, test.error);
+                        assert.equal(test.value, opt[test.param], 'Test that ' + test.param + ' is equal to ' + test.value + ' (actual is ' + opt[test.param] + ')');
                     });
-                })(data.bad[k]);
+                })(data.params[k]);
             }
 
         });
