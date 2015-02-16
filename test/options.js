50deb5c2a1013de08081322153dd70544f44f149
Ivan Wills <ivan.wills@gmail.com>
2015-02-17 10:01:56 +1100

    Started doing final implementation (not quite 100% coverage)

--- test/options.js
+++ test/options.js
@@ -26,12 +26,36 @@ var test_data = [
         ],
         'cmdline': [
             {
-                name: 'passed long option',
-                argv: ['--long']
+                name  : 'passed long option',
+                argv  : ['--long'],
+                params: { long: true }
             },
             {
-                name: 'passed short option',
-                argv: ['-l'    ]
+                name  : 'passed short option',
+                argv  : ['-l', 'a'],
+                extra : ['a'],
+                params: { long: true }
+            }
+        ],
+    },
+    {
+        'name'  : 'Object with value arguments',
+        'args'  : [
+            ['long|l', {description: 'A long named option'}],
+            ['int|i=i', {description: 'A long named option'}],
+        ],
+        'cmdline': [
+            {
+                name  : 'passed short option',
+                argv  : ['-l', '-i', '1'],
+                extra : [],
+                params: { long: true, int: 1 }
+            },
+            {
+                name  : 'passed short option',
+                argv  : ['-li1'],
+                extra : [],
+                params: { long: true, int: 1 }
             }
         ],
     }
@@ -46,6 +70,11 @@ for ( var i in test_data ) {
                         var error, opt, result;
                         try {
                             opt = getoptLong.configure(data.args);
+                            process.argv = test.argv;
+                            process.argv.unshift('node', 'test');
+                            if (test.hasOwnProperty('extra')) {
+                                test.extra.unshift('node', 'test');
+                            }
                             result = opt.process();
                             error = false;
                         }
@@ -53,9 +82,15 @@ for ( var i in test_data ) {
                             error = e;
                         }
                         if (error) {
-                            console.log('Error ', error, opt, result);
+                            console.log({error: error, opt: opt.parameters, params: result, argv: process.argv, test: test});
                         }
                         assert.equal(error, false, 'No error creating object');
+                        if (test.params) {
+                            assert.deepEqual(test.params, result, 'Get the expected params set');
+                        }
+                        if (test.extra) {
+                            assert.deepEqual(test.extra, process.argv, 'Get the expected leftover arguments set');
+                        }
                     });
                 })(data.cmdline[j]);
             }
@@ -68,7 +103,7 @@ for ( var i in test_data ) {
                             opt = getoptLong.configure(data.args);
                         }
                         catch (e) {
-                            error = e;
+                           error = e;
                         }
                         assert.true(!error, test.error);
                         assert.equal(test.value, opt[test.param], 'Test that ' + test.param + ' is equal to ' + test.value + ' (actual is ' + opt[test.param] + ')');
@@ -93,6 +128,8 @@ describe('Protections', function() {
         var error = false;
         try {
             var opt = getoptLong.configure([['log|l', { description: 'long option' }]]);
+            process.argv = ['node', 'test', '-l'];
+            opt.process();
         }
         catch (e) {
             error = e;
