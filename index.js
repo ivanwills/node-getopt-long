e9a0bb1da5544812aa51f038cb75b87793a209f1
Ivan Wills <ivan.wills@gmail.com>
2015-02-17 19:53:37 +1100

    Adding index.js file in the hope that this will allow my module to work as expected

---
+++ index.js
@@ -0,0 +1,8 @@
+
+var Param = require('../lib/getopt-long-param.js');
+var getoptLong = require('../lib/getopt-long.js');
+
+var exports;
+exports.get = getoptLong.get;
+exports.configure = getoptLong.configure;
+exports.param = Param.param;
