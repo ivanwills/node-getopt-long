2f61cfd9eced21e7543491d548906e5fcd13c684
Ivan Wills <ivan.wills@gmail.com>
2015-02-23 07:24:44 +1100

    Got test working

--- test/options-help.js
+++ test/options-help.js
@@ -4,10 +4,14 @@ var getoptLong = require('../lib/getopt-long.js');
 
 var test_data = [
     {
-        name  : '',
-        config: [
+        name  : 'Short config',
+        config: [[
+                ['long|l', "Long message"]
+            ],
+            {}
         ],
-        help: '',
+        help: '  test\n'
+            + '  -l --long     Long message\n'
     }
 ];
 
@@ -15,13 +19,14 @@ describe('Full help', function() {
     for (var i in test_data) {
         (function(test) {
             it(test.name, function() {
+                process.argv = ['node', 'test'];
                 try {
-                var opt = getoptLong.configure(test.config);
+                    var opt = getoptLong.configure.apply(this, test.config);
                 }
                 catch (e) {
                     console.log(e);
                 }
-                assert.equals(test.help, opt.help(), 'help generated correctly');
+                assert.equal(test.help, opt.help(), 'help generated correctly\n"' + test.help + '"\n"' + opt.help() + '"\n');
             });
         })(test_data[i]);
     }
