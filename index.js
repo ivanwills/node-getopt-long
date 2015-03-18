4e592aa009208fd9175b41b480644642edfb5910
Ivan Wills <ivan.wills@gmail.com>
2015-03-18 19:14:41 +1100

    Added missing export

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ var getoptLong = require('./lib/getopt-long.js');
 var exports;
 exports.get = getoptLong.get;
 exports.configure = getoptLong.configure;
+exports.options = getoptLong.options;
 exports.param = Param.param;
